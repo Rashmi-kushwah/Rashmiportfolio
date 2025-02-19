3049e6b70841c50d624d13cca6b417fa8670a8d4
Rashmi Kushwah <rashmiinfo6@gmail.com>
2025-02-19 18:13:09 +0530

    Your commit message

--- Aboutus/admin.py
+++ Aboutus/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from django.contrib import admin
+from .models import PortfolioImage
 from .models import Aboutus
 
 admin.site.register(Aboutus)
 
 
-from .models import PortfolioImage
 admin.site.register(PortfolioImage)
