017444cd5ed3d8edc38cd38aeef0c1719f56388c
Rashmi Kushwah <rashmiinfo6@gmail.com>
2025-02-19 19:35:29 +0530

    Updated ALLOWED_HOSTS for Vercel

--- Rashmi_portfolio/settings.py
+++ Rashmi_portfolio/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-&7jll*f(a-=hn^0*1r!p^c-h%h&%vsk3bbrgl2gkchwh*nafte
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = ['*', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
